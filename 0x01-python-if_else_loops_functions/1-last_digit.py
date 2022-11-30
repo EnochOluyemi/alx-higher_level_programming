97b6f90b10e499153f71257847c4092ee1cfd228
EnochOluyemi <morijin1246@gmail.com>
2022-11-30 02:18:50 +0100

    Initial commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,6 +7,6 @@ unit = int(unit)
 if unit > 5:
     print(f"Last digit of {number} is {unit} and is greater than 5")
 elif unit < 6 and unit > 0:
-    print(f"Last digit of {number} is {unit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{unit} and is less than 6 and not 0")
 else:
     print(f"Last digit of {number} is 0 and is 0")
